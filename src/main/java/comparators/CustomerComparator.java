be527d35b8ff86cef562492c118680185d27599b
Manuel Rodriguez <manuel.rodriguez@Manuel-Rodriguez-MacBook-Pro.local>
2022-12-06 18:34:50 -0600

    Added documentaton for CustomerComparator class

--- src/main/java/comparators/CustomerComparator.java
+++ src/main/java/comparators/CustomerComparator.java
@@ -4,7 +4,22 @@ import payload.BillingAddress;
 import payload.Customer;
 import payload.ShippingAddress;
 
+/**
+ * This class was made to compare objects returned by API calls against test parameters
+ * As a way to exemplofy the use of custom comparators.
+ */
 public class CustomerComparator {
+
+    /**
+     * Compare a Customer object with parameter values
+     * @param customer A customer object
+     * @param email Customer email
+     * @param first_name Customer first name
+     * @param last_name Customer last name
+     * @param username Customer username
+     * @param password Customer password
+     * @return True if all values match, false otherwise
+     */
     public boolean compareCustomer(Customer customer, String email, String first_name, String last_name, String username,
                                    String password) {
         boolean result = customer.getEmail().equals(email) &&
@@ -16,6 +31,22 @@ public class CustomerComparator {
 
     }
 
+    /**
+     * Compare a Billing Address object with parameter values
+     * @param billingAddress A Billing Address object
+     * @param first_name Address owner first name
+     * @param last_name Address owner last name
+     * @param company Address company
+     * @param address_1 Address line 1
+     * @param address_2 Address line 2
+     * @param city Address city name
+     * @param state Address state name
+     * @param postcode Address postal code
+     * @param country Address country
+     * @param email Address owner email
+     * @param phone Address owner phone
+     * @return True if all values match, false otherwise
+     */
     public boolean compareBillingAddress(BillingAddress billingAddress,
                                          String first_name, String last_name, String company,
                                          String address_1, String address_2, String city, String state,
@@ -35,6 +66,20 @@ public class CustomerComparator {
         return result;
     }
 
+    /**
+     * Compare a Shipping Address object with parameter values
+     * @param shippingAddress A Shipping Address object
+     * @param first_name Address owner first name
+     * @param last_name Address owner last name
+     * @param company Address company
+     * @param address_1 Address line 1
+     * @param address_2 Address line 2
+     * @param city Address city name
+     * @param state Address state name
+     * @param postcode Address postal code
+     * @param country Address country
+     * @return True if all values match, false otherwise
+     */
     public boolean compareShippingAddress(ShippingAddress shippingAddress,
                                           String first_name, String last_name, String company,
                                           String address_1, String address_2, String city, String state,
